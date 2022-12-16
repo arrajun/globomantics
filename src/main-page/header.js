59a3d7334b914bb5af5df1a5afc4fc4194bb11e9
Arjun Balasubramanian <arrajun610@gmail.com>
2022-12-16 16:29:43 +0530

    housejson

--- src/main-page/header.js
+++ src/main-page/header.js
@@ -1,13 +1,11 @@
 import GloboLogo from "./GloboLogo.png";
-const Header = () => {
+const Header = ({ subtitle, title }) => {
   return (
     <header className="row">
       <div className="col-md-5">
         <img src={GloboLogo} className="logo" alt="Globologo" />
       </div>
-      <div className="col-md-7 mt-5 subtitle">
-        Providing houses all over the world
-      </div>
+      <div className="col-md-7 mt-5 subtitle">{subtitle}</div>
     </header>
   );
 };
